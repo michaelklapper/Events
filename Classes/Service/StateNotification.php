4c3fab83d4c7534229f7ef0ce060afc9da8140e4
Mick <michael.klapper+github@gmail.com>
2011-06-20 19:07:32 +0200

    Add settings for StageNotification

--- Classes/Service/StateNotification.php
+++ Classes/Service/StateNotification.php
@@ -19,6 +19,12 @@ class StateNotification extends \F3\FLOW3\MVC\Controller\ActionController {
 	protected $logger;
 
 	/**
+	 * Contains the settings of the current package
+	 * @var array
+	 */
+	protected $settings;
+
+	/**
 	 * For logging we need a logger, which we will get injected automatically by
 	 * the Object Manager
 	 *
@@ -34,6 +40,18 @@ class StateNotification extends \F3\FLOW3\MVC\Controller\ActionController {
 	}
 
 	/**
+	 * Injects the settings of the package this controller belongs to.
+	 *
+	 * @param array $settings Settings container of the current package
+	 * @return void
+	 *
+	 * @author Michael Klapper <mick.klapper.development@gmail.com>
+	 */
+	public function injectSettings(array $settings) {
+		$this->settings = $settings;
+	}
+
+	/**
 	 * Before advice, logs all access to methods of our package
 	 *
 	 * @param \F3\FLOW3\AOP\JoinPointInterface $joinPoint: The current join point
@@ -42,12 +60,33 @@ class StateNotification extends \F3\FLOW3\MVC\Controller\ActionController {
 	 * @after method(F3\Events\Domain\Model\Event->setState())
 	 * @author Michael Klapper <mick.klapper.development@gmail.com>
 	 */
-	public function sendNotificationOnState(\F3\FLOW3\AOP\JoinPointInterface $joinPoint) {
+	public function sendNotificationOnStateCanceled(\F3\FLOW3\AOP\JoinPointInterface $joinPoint) {
 		$state = $joinPoint->getMethodArgument('state');
+		$proxy = $joinPoint->getProxy();
 		$message = '';
 
-		if ($state->getId() === 5) {
-			$message = 'Events state was changed to: ' . $state->getTitle();
+		if ($state->getId() === $this->settings['StageNotification']['canceled']) {
+			$message = 'CANCELED: Event "' . $proxy->getTitle() . '" state was changed to: ' . $state->getTitle();
+			$this->logger->log($message);
+		}
+	}
+
+	/**
+	 * Before advice, logs all access to methods of our package
+	 *
+	 * @param \F3\FLOW3\AOP\JoinPointInterface $joinPoint: The current join point
+	 * @return void
+	 *
+	 * @after method(F3\Events\Domain\Model\Event->setState())
+	 * @author Michael Klapper <mick.klapper.development@gmail.com>
+	 */
+	public function sendNotificationOnStateApproved(\F3\FLOW3\AOP\JoinPointInterface $joinPoint) {
+		$state = $joinPoint->getMethodArgument('state');
+		$proxy = $joinPoint->getProxy();
+		$message = '';
+
+		if ($state->getId() === $this->settings['StageNotification']['approved']) {
+			$message = 'APPROVED: Event "' . $proxy->getTitle() . '" state was changed to: ' . $state->getTitle();
 			$this->logger->log($message);
 		}
 	}
